22ca6d62625fd142d145b905d9148f58bec70205
Alyssa Davis <alyssadavis@alyss.local>
2013-10-31 18:49:35 -0400

    function to light rooms works

--- WEBinterface/cooperspaces.js
+++ WEBinterface/cooperspaces.js
@@ -1,9 +1,7 @@
 
 var init = function() {
-	
 	var elements = document.getElementsByClassName('layer');
 	var transparentbox = document.getElementById('transparentbox');
-    //var transparentbox = document.body;
 
     for (var i = 0; i < elements.length; i++) {
 	    elements[i].addEventListener( 'click', function(){
@@ -11,30 +9,41 @@ var init = function() {
 	    	var state = this.hasClassName('flipped');
 	    	var stateofbehindbox = transparentbox.hasClassName('transparentboxbehind');
 
-	    	console.log("state of behind box " + stateofbehindbox);
+	    	//console.log("state of behind box " + stateofbehindbox);
 		    turnoff();
 
 		    function turnoff(){
+		    	var z = 120;
 			    for (j = 0; j < elements.length; j++) {
+			    	
 			    	if (elements[j].hasClassName('flipped') == true) {
-			    		elements[j].toggleClassName('flipped');
+			    		elements[j].toggleClassName('flipped');	
 			    	}
+			    	elements[j].parentNode.style.zIndex = z;
+			    	z = z - 10;
+			    	//console.log("this is the element number: " + elements[j].childNodes[1].className + " This is its z index: " + z);
 			    }   
 	    	}
 	    	
 	    	
 	    	if (stateofbehindbox == true ) {
-	    		console.log( "the behind box is already open bc a neww floor was clicked from pov of other floor" );
+	    		//console.log( "the behind box is already open bc a neww floor was clicked from pov of other floor" );
 	    	} else if (stateofbehindbox == false ){
 	    		transparentbox.toggleClassName('transparentboxbehind');
 	    	}
 
 	    	if (state == true ) {
 	    		this.className.replace('flipped','');
-	    		console.log("the floor is open and since it was clicked remove the class and it will go back down");
+	    		//console.log("the floor is open and since it was clicked remove the class and it will go back down");
 	    		transparentbox.toggleClassName('transparentboxbehind');
 	    	} else if (state == false ){
-	    		this.toggleClassName('flipped', false);
+	    		this.toggleClassName('flipped');
+	    		//console.log(this.__proto__)
+
+	    		//this.addEventListener( 'webkitTransitionEnd', function( event ) { 
+	    		this.parentNode.style.zIndex = 1000; 
+	    		//console.log("this is the currently open floor: " + this.childNodes[1].className + " This is its z index: " + this.parentNode.style.zIndex);
+	    	//}, false);
 	    	}
 	    	
 	    	transparentbox.addEventListener( 'click', function(){
@@ -43,13 +52,71 @@ var init = function() {
 	    		console.log("the box was clicked");
 	    	}, false);
 
-	  	}, false);
+	  	}, false);	
   	}
 
   	transparentbox.addEventListener( 'click', function(){
   		console.log("the box was clicked but nothing should happen");
 	}, false);
 
-};
+  	var turnonlist = new Array();
+	
+	function checkstatus(t) {
+		turnonlist = [];
+		for (f = 0; f < 10; f++) {	
+			for (var key in buildingSnapshots[t][f]){
+				var obj = buildingSnapshots[t][f][key];
+				for (var prop in obj){
+					if (obj[prop] == true) {
+						turnonlist.push(key);
+					} 
+				}
+			}
+		}
+		//these are the rooms in the current time interval that need to be turned on
+		console.log(turnonlist);
+		//return turnonlist;
+		clearrooms();
+		updaterooms(turnonlist);
+	}
+
+	function clearrooms() {
+		for (i=1; i<=11; i++) {
+			var id = 'fl'+i;
+			id = id.toString();
+			var url = 'url(Images/floor'+i+'.png)';
+			//console.log(id);
+			//console.log(url);
+			document.getElementById(id).style.backgroundImage = url;
+		}
+	}
+
+	function updaterooms(turnonlist) {
+		for (i=0; i<turnonlist.length; i++) {
+			//console.log(turnonlist[i]);
+			var id = "fl" + turnonlist[i].charAt(4);
+			id = id.toString();
+			var url = 'url(Images/' + turnonlist[i] + 'on.png), ';
+			//console.log(id);
+			//console.log(url);
+			var currentstyle = document.getElementById(id).style.backgroundImage;
+			document.getElementById(id).style.backgroundImage = url + currentstyle; 
+		}	
+	}
+
+	checkstatus(0);
+}
+
+
+
+
+
+
 
 window.addEventListener('DOMContentLoaded', init, false);
+
+
+
+
+
+
--- WEBinterface/data.js
+++ WEBinterface/data.js
@@ -1,3 +1,85 @@
-var occupiedSlots = {
-	
-};
+var buildingSnapshots = [
+	//building for one interval
+	[
+		//floor 1
+		{
+			room101: {
+				name:"101", 
+				occupied: true,
+				occupancy:32
+			},
+			room104: {
+				name:"104", 
+				occupied: true,
+				occupancy:12 
+			},
+			room105: {
+				name:"105", 
+				occupied: true,
+				occupancy:2
+			},
+			room106: {
+				name:"106", 
+				occupied: false,
+				occupancy:60
+			},
+
+		},
+		//floor 2
+		{
+			room201: {
+				name:"201", 
+				occupied: true,
+				occupancy:32 
+			},
+			room202: {
+				name:"201A", 
+				occupied: true,
+				occupancy:12
+			}
+		}
+	],
+	//building for next interval
+	[
+		{
+			room101: {
+				name:"101", 
+				occupied: false,
+				occupancy:32 
+			},
+			room104: {
+				name:"104", 
+				occupied: false,
+				occupancy:12 
+			},
+			room105: {
+				name:"105", 
+				occupied: false,
+				occupancy:2
+			},
+			room106: {
+				name:"106", 
+				occupied: false,
+				occupancy:60
+			}
+
+		},
+		{
+			room201: {
+				name:"201", 
+				occupied: false,
+				occupancy:32
+			},
+			room202: {
+				name:"201A", 
+				occupied: false,
+				occupancy:12 
+			}
+		}
+	]	
+];
+
+
+
+
+
