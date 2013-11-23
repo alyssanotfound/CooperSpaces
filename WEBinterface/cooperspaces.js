b855a0bcf441cf33fb840bb91db7efa3436069c8
Alyssa Davis <alyssadavis@alyss.local>
2013-11-23 12:48:38 -0500

    open close interface working

--- WEBinterface/cooperspaces.js
+++ WEBinterface/cooperspaces.js
@@ -1,151 +1,192 @@
-
+//currentLayer keeps track of the floor that is open and gets deleted when a floor is closed
+var currentLayer = "";
+var currentFloor;
 var elements = new Array();
 var transparentbox;
 var turnonlist = new Array();
+var stateofbehindbox;
+var moveAbove = new Array();
+var moveBelow = new Array();
 
 //this function init gets called once the page loads
 var init = function() {
 	elements = document.getElementsByClassName('layer');
-	//console.log(elements.length);
-	//console.log(elements[0]);
 	transparentbox = document.getElementById('transparentbox'); 
+	stateofbehindbox = getstateofbehindbox();
+	//console.log( stateofbehindbox );
 	checkstatus(1);
 	addEL(elements);
-	addBoxEL(transparentbox);
+}
+
+var getstateofbehindbox = function() {
+	return transparentbox.hasClassName('transparentboxbehind');
 }
 
 function addEL(elements) {
 	for (var i = 0; i < elements.length; i++) {
-		//console.log(elements.length);
 	    elements[i].addEventListener( 'click', function() {
-			console.log(this);
-			var currentLayer = this;
-			var currentFloor = this.childNodes[1].className;
-			currentFloor = currentFloor.toString();
-			
-			if (currentFloor.length == 3) {
-				currentFloor = Number(currentFloor[2]);	
-			} else if (currentFloor.length == 4) {
-				currentFloor = currentFloor.substr(2,3);
-				currentFloor = Number(currentFloor);
+	    	console.log ("--------------NEW THING CLICKED---------------")
+	    	//console.log("This should be blank if nothing is open or show the room if one is open: " + currentFloor);
+	    	if (currentLayer === this) {
+	    		//console.log("I just clicked the floor already open, thus, do nothing")
+	    		//do nothing, because you dont want to close out of the open floor if user clicks on it again
+	    	} else {
+	    		//if there is an open one, close it 
+	    		if (currentLayer != ""){
+	    			//console.log("There must be this one open already so now close it...." + currentFloor)
+	    			FlipDown(currentLayer, currentFloor);
+				} 
+				//console.log("...now open the new floor")
+				//if there isn't an open one, open new one
+				currentLayer = this;
+				currentFloor = this.childNodes[1].className;
+				currentFloor = currentFloor.toString();
+				console.log("the currentLayer floor is: " + currentFloor);
+				
+				if (currentFloor.length == 3) {
+					currentFloor = Number(currentFloor[2]);	
+				} else if (currentFloor.length == 4) {
+					currentFloor = currentFloor.substr(2,3);
+					currentFloor = Number(currentFloor);
+				}
+				console.log(currentFloor);
+				FlipUp(currentLayer, currentFloor);
 			}
-			console.log(currentFloor);
-			FlipUpDown(currentLayer, currentFloor);
-
 		}, false);	
 	}
 }
 
-function FlipUpDown(currentLayer, currentFloor) {
-	var moveUp = new Array();
-	var moveDown = new Array();
-	var state = currentLayer.hasClassName('flipped');
-	var stateofbehindbox = transparentbox.hasClassName('transparentboxbehind');
-
-	turnoff();
-
+function defineMovingFloors(currentFloor){
+	console.log ("DEFINING THE FLOORS THAT WILL MOVE")
+	moveAbove.length = 0;
+	moveBelow.length = 0;
 	//finding the arrays of the floors that need to move up and that need to move down
 	if (currentFloor == 10) {
-		moveUp = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		moveDown = [11];
+		moveAbove = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		moveBelow = [11];
 	} else if (currentFloor == 11) {
-		moveUp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		moveAbove = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	} else {
 		for (var j=(currentFloor + 1); j <= 9; j++){
-			moveUp.push(j);
+			moveAbove.push(j);
 		}
 		for (var k=(currentFloor - 1); k > 0; k=k-1){
 			//console.log(k);
-			moveDown.push(k);
+			moveBelow.push(k);
 		}
-		moveDown.push(10, 11);	
+		moveBelow.push(10, 11);	
 	}
+}
 
-	MoveAway(moveUp, moveDown);
-
-	if (stateofbehindbox == true ) {
-		//console.log( "the behind box is already open bc a neww floor was clicked from pov of other floor" );
-	} else if (stateofbehindbox == false ){
-		transparentbox.toggleClassName('transparentboxbehind');
+function MoveAway(moveAbove, moveBelow) {
+	console.log ("FLOORS MOVE AWAY")
+	for (var l = 0; l < moveAbove.length; l++) {
+		var floor = Number(findelementnumber(moveAbove[l]));
+		elements[floor].parentNode.toggleClassName('toggleMoveUp');	
+		//console.log("move up element " + l);
 	}
-	console.log("the state is : " + state);
-	if (state == true ) {
-		currentLayer.className.replace('flipped','');
-		currentLayer.toggleClassName('unflipped');
-		currentLayer.parentNode.className.replace('flippedBox','');
-		currentLayer.parentNode.toggleClassName('unflippedBox');
-		//console.log("the floor is open and since it was clicked remove the class and it will go back down");
-		transparentbox.toggleClassName('transparentboxbehind');
-		MoveTowards(moveUp, moveDown);
-		turnoff();
-	} else if (state == false ){
-		currentLayer.toggleClassName('flipped');
-		currentLayer.parentNode.toggleClassName('flippedBox');
-		//console.log("this is the currently open floor: " + this.childNodes[1].className + " This is its z index: " + this.parentNode.style.zIndex);
+	for (var m = 0; m < moveBelow.length; m++) {
+		var floor = Number(findelementnumber(moveBelow[m]));	
+		elements[floor].parentNode.toggleClassName('toggleMoveDown');	
 	}
+}
 
+function FlipUp(currentLayer, currentFloor) {
+	
+	defineMovingFloors(currentFloor);
+	console.log("OPENING above: " + moveAbove);
+	console.log("below: " + moveBelow);
+	MoveAway(moveAbove, moveBelow);
+	console.log ("CURRENT FLOOR FLIPS UP")
+	//turn on big background box
+	transparentbox.toggleClassName('transparentboxbehind');
+	currentLayer.toggleClassName('flipped');
+	currentLayer.parentNode.toggleClassName('flippedBox');
+	
 	transparentbox.addEventListener( 'click', function(){
-		transparentbox.toggleClassName('transparentboxbehind');	
+		//trigger the open floor to go down if outside is clicked
+		console.log("CLICKED OUTSIDE BOX SO FLIP DOWN : " + currentLayer + currentFloor);
 		turnoff();
-		//console.log("the box was clicked");
+		//FlipDown(currentLayer, currentFloor);
 	}, false); 
 }
 
-function MoveAway(moveUp, moveDown) {
+//will collapse the open floor and reset everything
+function FlipDown(currentLayer, currentFloor) {
 	
+	//console.log("inside FlipDown " + currentLayer + currentFloor);
+	defineMovingFloors(currentFloor);
+	console.log ("CURRENT FLOOR FLIPS DOWN ")
+	currentLayer.className.replace('flipped','');
+	currentLayer.toggleClassName('unflipped');
+	currentLayer.parentNode.className.replace('flippedBox','');
+	currentLayer.parentNode.toggleClassName('unflippedBox');
+	//console.log("the floor is open and since it was clicked remove the class and it will go back down");
+	transparentbox.toggleClassName('transparentboxbehind');
+	console.log("CLOSING above: " + moveAbove);
+	console.log("below: " + moveBelow);
 
-	for (var l = 0; l < moveUp.length; l++) {
-		elements[l].parentNode.toggleClassName('toggleMoveUp');	
-		console.log(l);
+	MoveTowards(moveAbove, moveBelow);
+}
+
+function MoveTowards(moveAbove, moveBelow) {
+	console.log ("FLOORS MOVE TOWARDS")
+	for (var m = 0; m < moveAbove.length; m++) {
+		var floor = Number(findelementnumber(moveAbove[m]));
+		//console.log("move up " + floor);
+		elements[floor].parentNode.toggleClassName('toggleMoveUp');	
 	}
-
-	for (var m = 0; m < moveDown.length; m++) {
-		var floor = Number(findelementnumber(moveDown[m]));
-		
+	for (var l = 0; l < moveBelow.length; l++) {
+		var floor = Number(findelementnumber(moveBelow[l]));
+		//console.log("move down " + floor);
 		elements[floor].parentNode.toggleClassName('toggleMoveDown');	
 	}
-
+	
+	turnoff();
 }
 
-function MoveTowards(moveUp, moveDown) {
-	for (var l = 0; l < moveDown.length; l++) {
-		//elements[l].parentNode.className.replace('toggleMoveDown','');
-		elements[l].parentNode.toggleClassName('toggleMoveUp');	
-	}
-	for (var m = 0; m <= moveUp.length; m++) {
-		var floor = Number(findelementnumber(moveUp[m]));
-		console.log(floor);
-		//elements[l].parentNode.className.replace('toggleMoveUp','');
-		elements[floor].parentNode.toggleClassName('toggleMoveDown');	
-	}
-}
-
-function addBoxEL(transparentbox) {
-		transparentbox.addEventListener( 'click', function(){
-			//console.log("the box was clicked but nothing should happen");
-	}, false);	
-}
-
-//reset all of the floors so that none are in the foreground
+//reset all of the floors so that none are in the foreground though this is really just a safety
 function turnoff(){
+	console.log ("EVERYTHING IS RESET")
 	var z = 120;
     for (j = 0; j < elements.length; j++) {
     	
     	if (elements[j].hasClassName('flipped') == true) {
     		elements[j].toggleClassName('flipped');	
     		elements[j].parentNode.toggleClassName('flippedBox');
-    	}
+    	} 
     	if (elements[j].hasClassName('unflipped') == true) {
     		elements[j].toggleClassName('unflipped');	
     		elements[j].parentNode.toggleClassName('unflippedBox');
-    	}
-    	elements[j].parentNode.style.zIndex = z;
-    	z = z - 10;	
-    }		      
+    	} 
+    	if (elements[j].parentNode.hasClassName('toggleMoveUp') == true) {
+    		elements[j].parentNode.toggleClassName('toggleMoveUp');
+    	} 
+		if (elements[j].parentNode.hasClassName('toggleMoveDown') == true) {
+    		elements[j].parentNode.toggleClassName('toggleMoveDown');
+    	} 
+    	elements[j].parentNode.style.zIndex = "";
+    		
+    }	
+    if (transparentbox.hasClassName('transparentboxbehind') == true){
+    	transparentbox.toggleClassName('transparentboxbehind');	
+    }	
+	currentLayer = "";
+	currentFloor = "";	
+	moveAbove.length = 0;
+	moveBelow.length = 0;
+
+	console.log("these are the elements: " + elements);
+	console.log("these move above: " + moveAbove);
+	console.log("these move below: " + moveBelow);
+	console.log("this is the state of the behind box: " + stateofbehindbox);
+	console.log("current layer : " + currentLayer);
+	console.log("current floor : " + currentFloor);
+
 }
 
 function findelementnumber(i){
-	if (i == 9) {
+	if (i == "9") {
 		return 0;
 	} else if (i == "8") {
 		return 1;
@@ -169,8 +210,6 @@ function findelementnumber(i){
 		return 10;
 	}
 }
-
-
 
 //clears all of the background images on all of the floor divs except for the basic floor images
 function clearrooms() {
